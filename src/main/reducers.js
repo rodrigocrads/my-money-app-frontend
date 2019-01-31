64b2346587b34ca35bf99f3066b518dcd4a09f5c
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2019-01-31 15:33:09 -0200

    feat(Redux): Configurando Redux

---
+++ src/main/reducers.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+    dashboard: () => ({
+        sumary: {
+            credit: 100, debt: 50
+        }
+    })
+})
+
+export default rootReducer
