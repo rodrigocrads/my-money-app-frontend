2fae17cd6072b9e3aad9845c113ebb65e80e7042
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2019-02-04 21:24:23 -0200

    feat(BillingCycle): Definition the class Billing Cycle

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-    <div>
-        <h1> Ciclos de Pagamentos</h1>
-    </div>
-)
+import Content from '../common/template/content'
+import ContentHeader from '../common/template/contentHeader'
+
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de pagamento' small='Cadastro' />
+                <Content>
+                    Ciclos de Pagamento
+                </Content>
+            </div>
+        );
+    }
+}
+
+export default BillingCycle
