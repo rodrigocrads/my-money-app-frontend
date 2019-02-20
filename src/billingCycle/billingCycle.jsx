98f4a07cd7c7d76cf3ebcf18010856162fe85cfb
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2019-02-19 22:29:41 -0300

    feat(BillingCycle): create action for insert billing cycle and reducer

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -11,6 +11,8 @@ import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from '../common/tab/tabActions'
 import List from './billingCycleList'
+import Form from './BillingCycleForm'
+import { create } from './billingCycleActions'
 
 class BillingCycle extends Component {
     componentWillMount(){
@@ -36,7 +38,7 @@ class BillingCycle extends Component {
                             </TabContent>
 
                             <TabContent id='tabCreate'>
-                                <h1>Incluir</h1>
+                                <Form onSubmit={ this.props.create } />
                             </TabContent>
 
                             <TabContent id='tabUpdate'>
@@ -54,5 +56,7 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ 
+    selectTab, showTabs, create 
+}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
