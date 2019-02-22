2e5b4b427d0fc4a7c069120a8ce57e32a89091a7
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2019-02-22 20:59:05 -0300

    feature(BillingCycleAction): implement messages for request and implement middleware redux-multi and redux-thunk

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
 
@@ -11,8 +14,21 @@ export function getList() {
 }
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values);
-    return {
-        type: 'TEMP',
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação realizada com sucesso')
+                dispatch([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList', 'tabCreate')
+                ]);
+            })
+            .catch(e => {
+                Object.values(e.response.data.errors)
+                    .forEach((error) => toastr.error('Erro', error.message));
+            });
     }
+    
 }
