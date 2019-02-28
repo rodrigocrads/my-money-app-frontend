5fa8eb05f5fb28ca73cf4f7b15f3ca4f9e0a3aa8
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2019-02-27 21:35:34 -0300

    feat(BillingCycle/CreditList): criado os botoes de adicionar e clonar item de credito, foi utilizado um action creator chamado arrayInsert

--- src/billingCycle/creditList.jsx
+++ src/billingCycle/creditList.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-import { Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+import { Field,arrayInsert } from 'redux-form';
 import Grid from '../common/layout/grid';
 import Input from '../common/form/input';
 
 class CreditList extends Component {
+
+    add(index, item= {}) {
+        if(!this.props.readOnly) {
+            this.props.arrayInsert('billingCycleForm', 'credits' , index, item);
+        }
+    }
 
     renderRows() {
         const list = this.props.list || [];
@@ -18,6 +26,17 @@ class CreditList extends Component {
                         <Field name={`credits[${index}].value`} component={Input} 
                             placeholder='Informe o nome' readOnly={this.props.readOnly} 
                         />
+                    </td>
+                    <td>
+                        <button type='button' className='btn btn-success'
+                            onClick={() => this.add(index + 1)}>
+                            <i className='fa fa-plus'></i>
+                        </button>
+
+                        <button type='button' className='btn btn-warning'
+                            onClick={() => this.add(index + 1, item)}>
+                            <i className='fa fa-clone'></i>
+                        </button>
                     </td>
                 </tr>
             )
@@ -47,4 +66,5 @@ class CreditList extends Component {
     }
 }
 
-export default CreditList;
+const mapDispatchToProps = dispatch => (bindActionCreators({arrayInsert}, dispatch));
+export default connect(null, mapDispatchToProps)(CreditList);
